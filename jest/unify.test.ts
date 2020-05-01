1d931b5c28cf2201d96368fbd23caf7b324a1ea3
gamburgm <mitch.gamburg@gmail.com>
2020-04-30 22:39:59 -0400

    added some functionality

--- jest/unify.test.ts
+++ jest/unify.test.ts
@@ -1,5 +1,5 @@
-import { selectMultiEquation, ERRORS, enqueue, dequeue } from '../src/unify';
-import { Null, MultiEquation, List, U, MultiVar, MultiTerm, Queue, Cons } from '../src/types';
+import { selectMultiEquation, ERRORS, enqueue, dequeue, reduce, createQueue, mergeMultiTerms, mergeMeq } from '../src/unify';
+import { Null, MultiEquation, List, U, MultiVar, MultiTerm, Queue, Cons, Pointer, TempMeq } from '../src/types';
 
 function makeList<T>(arr: Array<T>): List<T> {
   return arr.reduce((rest: List<T>, elem: T) => {
@@ -109,7 +109,7 @@ describe('Queue Functions', () => {
 
   describe('dequeue', () => {
     it('blows up on empty queue', () => {
-      expect(() => dequeue(Q)).toThrowError(EMPTY_QUEUE_ERROR);
+      expect(() => dequeue(Q)).toThrowError(ERRORS.EMPTY_QUEUE_ERROR);
     });
 
     it('pops an element off', () => {
@@ -171,21 +171,25 @@ describe('unification', () => {
   let VAR_TWO: MultiVar;
   let VAR_THREE: MultiVar;
   let VAR_FOUR: MultiVar;
-  let VAR_FIVE: MultiVar;
-  let VAR_SIX: MultiVar;
 
   let TERM_ONE: MultiTerm;
   let TERM_TWO: MultiTerm;
   let TERM_THREE: MultiTerm;
   let TERM_FOUR: MultiTerm;
+  let TERM_FIVE: MultiTerm;
+  let TERM_SIX: MultiTerm;
+  let TERM_SEVEN: MultiTerm;
+  let TERM_EIGHT: MultiTerm;
+
+  let PTR: Pointer<List<TempMeq>>;
 
   beforeEach(() => {
     VAR_ONE   = createVar(1);
     VAR_TWO   = createVar(2);
     VAR_THREE = createVar(3);
     VAR_FOUR  = createVar(4);
-    VAR_FIVE  = createVar(5);
-    VAR_SIX   = createVar(6);
+
+    PTR = { val: NULL };
 
     TERM_ONE = {
       fsymb: 'f',
@@ -229,6 +233,54 @@ describe('unification', () => {
       fsymb: 'y',
       args: NULL,
     };
+
+    TERM_FIVE = {
+      fsymb: 'q',
+      args: makeList([
+        {
+          S: EMPTY_QUEUE,
+          M: {
+            fsymb: 'z',
+            args: makeList([
+              {
+                S: makeQueue([VAR_ONE]),
+                M: { fsymb: 'v', args: NULL }
+              },
+            ]),
+          },
+        }
+      ]),
+    };
+
+    TERM_SIX = {
+      fsymb: 'z',
+      args: makeList([
+        {
+          S: makeQueue([VAR_ONE, VAR_TWO]),
+          M: { fsymb: 'v', args: NULL },
+        },
+      ]),
+    };
+
+    TERM_SEVEN = {
+      fsymb: 'g',
+      args: makeList([
+        {
+          S: makeQueue([VAR_TWO]),
+          M: null,
+        },
+      ]),
+    };
+
+    TERM_EIGHT = {
+      fsymb: 'z',
+      args: makeList([
+        {
+          S: makeQueue([VAR_THREE]),
+          M: { fsymb: 'v', args: NULL },
+        },
+      ]),
+    };
   });
 
   describe('selectMultiEquation', () => {
@@ -257,6 +309,94 @@ describe('unification', () => {
     it('does not contain the returned multiequation any longer', () => {
       selectMultiEquation(U);
       expect((U.zeroCount as Cons<MultiEquation>).value).toEqual(createMeq([VAR_ONE], TERM_ONE, 0));
+    });
+  });
+
+  describe('reduce', () => {
+    it('does not extract a frontier if args of multiterm empty', () => {
+      reduce(TERM_FOUR, PTR);
+      expect(PTR).toEqual({ val: NULL });
+    });
+
+    it('extracts a simple frontier from a multiterm with arguments', () => {
+      reduce(TERM_THREE, PTR);
+      expect(PTR.val).toEqual(makeList([
+        {
+          S: makeQueue([VAR_FOUR]),
+          M: null,
+        },
+      ]));
+    });
+
+    it('extracts the common part of the arguments of a multiterm', () => {
+      reduce(TERM_THREE, PTR);
+      expect((TERM_THREE.args as Cons<TempMeq>).value.S).toEqual({ push: null, pop: null });
+      expect(((TERM_THREE.args as Cons<TempMeq>).rest as Cons<TempMeq>).value.S).toEqual(createQueue(VAR_FOUR));
+    });
+
+    it('reduces a tempmeq with no LHS', () => {
+      reduce(TERM_FIVE, PTR);
+      expect(PTR.val).toEqual(makeList([
+        {
+          S: makeQueue([VAR_ONE]),
+          M: { fsymb: 'v', args: NULL },
+        },
+      ]));
+    });
+
+    it('reduces a multiterm which equates variables to symbols', () => {
+      reduce(TERM_SIX, PTR);
+      expect(PTR.val).toEqual(makeList([
+        {
+          S: makeQueue([VAR_ONE, VAR_TWO]),
+          M: { fsymb: 'v', args: NULL },
+        },
+      ]));
+    });
+
+    it('reduces a multiterm and determines the common part', () => {
+      reduce(TERM_SIX, PTR);
+      expect((TERM_SIX.args as Cons<TempMeq>).value.S).toEqual(createQueue(VAR_TWO));
+    });
+  });
+
+  describe('mergeMultiTerms', () => {
+    it('returns the second multiterm if first one is empty', () => {
+      expect(mergeMultiTerms(null, TERM_ONE)).toEqual(TERM_ONE);
+    });
+
+    it('blows up if function symbols do not match', () => {
+      expect(() => mergeMultiTerms(TERM_ONE, TERM_TWO)).toThrowError(ERRORS.DIFF_FUNCS);
+    });
+
+    it('combines the vars across multiple arguments of terms', () => {
+      expect(mergeMultiTerms(TERM_TWO, TERM_SEVEN)).toEqual({
+        fsymb: 'g',
+        args: makeList([
+          {
+            S: makeQueue([VAR_THREE, VAR_TWO]),
+            M: null,
+          },
+        ]),
+      });
+    });
+
+    it('merges the multiterms together', () => {
+      expect(mergeMultiTerms(TERM_SIX, TERM_EIGHT)).toEqual({
+        fsymb: 'z',
+        args: makeList([
+          {
+            S: makeQueue([VAR_ONE, VAR_TWO, VAR_THREE]),
+            M: { fsymb: 'v', args: NULL },
+          },
+        ]),
+      });
+    });
+  });
+
+  describe('mergeMeq', () => {
+    it('does nothing on empty multiequations', () => {
+
     });
   });
 });
--- src/unify.ts
+++ src/unify.ts
@@ -1,9 +1,23 @@
-import { U, MultiEquation, MultiTerm, List, TempMeq, Pointer, MultiVar, Queue, Node } from './types';
+import { U, MultiEquation, MultiTerm, List, TempMeq, Pointer, MultiVar, Queue, Node, Cons } from './types';
 
 export const ERRORS: Record<string, string> = {
   NO_MULTS: 'Error: No Multiequations to return!',
   EMPTY_QUEUE_ERROR: 'Queue is empty!',
+  DIFF_FUNCS: 'Function symbols are not equal!',
 };
+
+export function append<T>(pushQ: Queue<T>, receiveQ: Queue<T>): void {
+  if (!receiveQ.push) {
+    receiveQ.push = pushQ.push;
+    receiveQ.pop = pushQ.pop;
+  } else {
+    if (pushQ.pop) {
+      receiveQ.push.prev = pushQ.pop;
+      pushQ.pop.next = receiveQ.push;
+      receiveQ.push = pushQ.push;
+    }
+  }
+}
 
 export function enqueue<T>(q: Queue<T>, elem: T): void {
   const node: Node<T> = { data: elem, next: null, prev: null };
@@ -64,5 +78,49 @@ export function reduce(M: MultiTerm, ptr: Pointer<List<TempMeq>>): void {
     }
 
     currArg = currArg.rest;
+  }
+}
+
+export function mergeMultiTerms(M1: MultiTerm, M2: MultiTerm): MultiTerm {
+  if (!M1) return M2;
+  if (M2) {
+    if (M1.fsymb !== M2.fsymb) throw ERRORS.DIFF_FUNCS;
+    let args1 = M1.args;
+    let args2 = M2.args;
+
+    while (args1.empty === false) {
+      append(args1.value.S, (args2 as Cons<TempMeq>).value.S);
+      (args2 as Cons<TempMeq>).value.M = mergeMultiTerms(args1.value.M, (args2 as Cons<TempMeq>).value.M);
+      args1 = args1.rest;
+      args2 = (args2 as Cons<TempMeq>).rest;
+    }
+  }
+
+  return M2;
+}
+
+export function mergeMeq(M1: MultiEquation, M2: MultiEquation, U: U): MultiEquation {
+  if (M1 !== M2) {
+    // TODO I don't know if this works
+    if (M1.varnum < M2.varnum) {
+      const swap = M1;
+      M1 = M2;
+      M2 = swap;
+    }
+
+    M1.counter += M2.counter;
+    M1.varnum += M2.varnum;
+    let otherVars: List<MultiVar> = M2.S;
+
+    while (otherVars.empty === false) {
+      const V = otherVars.value;
+      otherVars = otherVars.rest;
+      V.M = M1;
+      M1.S = { empty: false, value: V, rest: M1.S };
+    }
+
+    U.meqNum -= 1;
+    M1.M = mergeMultiTerms(M1.M, M2.M);
+    return M1;
   }
 }
